1f7995b3b8eb284c5fa0b41b8abe48b91a85ec01
Michal Krulich <xkruli03@vutbr.cz>
2024-02-27 11:04:43 +0100

    feat: entities, enums

---
+++ project.DAL/Entities/StudentEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project.DAL.Entities;
+
+public record StudentEntity : IEntity
+{
+    public required Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string Surname { get; set; }
+    public string? ImageUrl { get; set; }
+    public ICollection<SubjectEntity> Subjects { get; set; } = new List<SubjectEntity>();
+    public ICollection<RatingEntity> Ratings { get; set; } = new List<RatingEntity>();
+}
---
+++ project.DAL/Entities/SubjectEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project.DAL.Entities;
+
+public record SubjectEntity : IEntity
+{
+    public required Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string Acronym { get; set; }
+    public ICollection<StudentEntity> Students { get; set; } = new List<StudentEntity>();
+    public ICollection<ActivityEntity> Activities { get; set; } = new List<ActivityEntity>();
+}

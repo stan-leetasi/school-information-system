d0c5a9adad7d561a12b1572d0942d27d63068f43
Michal Krulich <xkruli03@vutbr.cz>
2024-03-08 09:42:05 +0100

    feat: repository with mappers

---
+++ project.DAL/Mappers/StudentSubjectEntityMapper.cs
@@ -0,0 +1,12 @@
+﻿using project.DAL.Entities;
+
+namespace project.DAL.Mappers;
+
+public class StudentSubjectEntityMapper : IEntityMapper<StudentSubjectEntity>
+{
+    public void MapToExistingEntity(StudentSubjectEntity existingEntity, StudentSubjectEntity newEntity)
+    {
+        existingEntity.StudentId = newEntity.StudentId;
+        existingEntity.SubjectId = newEntity.SubjectId;
+    }
+}
---
+++ project.DAL/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+using project.DAL.Entities;
+
+namespace project.DAL.Repositories;
+
+public interface IRepository<TEntity>
+    where TEntity : class, IEntity
+{
+    IQueryable<TEntity> Get();
+    void Delete(Guid entityId);
+    ValueTask<bool> ExistsAsync(TEntity entity);
+    Task<TEntity> InsertAsync(TEntity entity);
+    Task<TEntity> UpdateAsync(TEntity entity);
+}

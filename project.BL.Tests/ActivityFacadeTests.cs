9d0789f6d9be3f6bf5ccbff966c8d2fd9bba613f
Stanislav Letaši <135342903+stan-leetasi@users.noreply.github.com>
2024-04-05 16:14:49 +0200

    test: added tests for ActivityFacade and StudentFacade

--- project.BL.Tests/ActivityFacadeTests.cs
+++ project.BL.Tests/ActivityFacadeTests.cs
@@ -8,6 +8,7 @@ using Xunit;
 using Xunit.Abstractions;
 using System.Collections.Generic;
 using System.Diagnostics;
+using project.Common.Enums;
 
 namespace project.BL.Tests;
 
@@ -44,7 +45,7 @@ public sealed class ActivityFacadeTests : FacadeTestsBase
     }
 
     [Fact]
-    public async Task Create_New_Activity()
+    public async Task Create_New_Activity_For_Existing_Subject()
     {
         // Arrange
         ActivityAdminDetailModel activity = ActivityAdminDetailModel.Empty with
@@ -60,4 +61,169 @@ public sealed class ActivityFacadeTests : FacadeTestsBase
         ActivityAdminDetailModel? detailModel = await _activityFacadeSUT.GetAsync(activity.Id);
         Assert.Equal("Final Exam", detailModel.Description);
     }
+
+    [Fact]
+    public async Task Create_New_Activity_For_Non_Existing_Subject()
+    {
+        var NonExistingSubjectId = new Guid();
+        // Arrange
+        ActivityAdminDetailModel activity = ActivityAdminDetailModel.Empty with
+        {
+            SubjectId = NonExistingSubjectId,
+            Description = "Final Exam"
+        };
+
+        //Act
+        await Assert.ThrowsAsync<DbUpdateException>(async () =>
+        {
+            await _activityFacadeSUT.SaveAsync(activity);
+        });
+
+    }
+
+    [Fact]
+    public async Task Remove_Activity()
+    {
+        var ICSCvikoId = ActivitiesSeeds.ICSCviko.Id;
+
+        await _activityFacadeSUT.DeleteAsync(ICSCvikoId);
+
+        var RemovedActivity = await _activityFacadeSUT.GetAsync(ICSCvikoId);
+
+        Assert.Null(RemovedActivity);
+    }
+
+    [Fact]
+    public async Task Remove_Non_xisting_Activity()
+    {
+        // Arrange
+        var NonExistingActivityID = new Guid();
+
+        // Act & Assert
+        var exception = await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+        {
+            await _activityFacadeSUT.DeleteAsync(NonExistingActivityID);
+        });
+
+        Assert.Equal($"Sequence contains no elements", exception.Message);
+    }
+
+    [Fact]
+    public async Task Register_Student_For_Activity()
+    {
+        // Arrange
+        var subjectId = SubjectSeeds.ICS.Id;
+        var activityId = ActivitiesSeeds.ICSCviko.Id;
+        var studentId = StudentSeeds.Terry.Id;
+
+        // Act
+        await _activityFacadeSUT.RegisterStudent(activityId, studentId);
+
+        var TerryICSCviko = await _activityFacadeSUT.GetAsyncStudentDetail(activityId, studentId);
+
+        //Assert
+        Assert.NotNull(TerryICSCviko);
+        Assert.True(TerryICSCviko!.IsRegistered);
+        Assert.Equal(SubjectSeeds.ICS.Id, TerryICSCviko.SubjectId);
+        Assert.Equal(SubjectSeeds.ICS.Name, TerryICSCviko.SubjectName);
+        Assert.Equal(0, TerryICSCviko.Points);
+        Assert.Equal("", TerryICSCviko.Notes);
+        Assert.Equal(ActivityType.Exercise,TerryICSCviko.Type);
+    }
+
+    [Fact]
+    public async Task Register_Student_For_Activity_He_Is_Registered_For()
+    {
+        // Act & Assert
+        var exception = await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+        {
+            await _activityFacadeSUT.RegisterStudent(ActivitiesSeeds.ICSCviko.Id, StudentSeeds.John.Id);
+        });
+
+        Assert.Equal("Student is already registered.", exception.Message);
+    }
+
+    [Fact]
+    public async Task Register_NonExisting_Student_For_Activity()
+    {
+        // Arrange
+
+        var NonExistingStudent = new Guid();
+
+        // Act & Assert
+        var exception = await Assert.ThrowsAsync<ArgumentException>(async () =>
+        {
+            await _activityFacadeSUT.RegisterStudent(ActivitiesSeeds.ICSCviko.Id, NonExistingStudent);
+        });
+
+        Assert.Equal($"Student with {NonExistingStudent} does not exist.", exception.Message);
+    }
+
+    [Fact]
+    public async Task Unregister_Student_From_Activity()
+    {
+        // Act
+        await _activityFacadeSUT.UnregisterStudent(ActivitiesSeeds.ICSCviko.Id, StudentSeeds.John.Id);
+
+        //Assert
+        await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+        {
+            await _activityFacadeSUT.GetAsyncStudentDetail(ActivitiesSeeds.ICSCviko.Id, StudentSeeds.John.Id);
+        });
+    }
+
+    [Fact]
+    public async Task Unregister_Student_From_Activity_He_Is_Not_Registered_For()
+    {
+        // Act & Assert
+        var exception = await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+        {
+            await _activityFacadeSUT.UnregisterStudent(ActivitiesSeeds.ICSCviko.Id, StudentSeeds.Terry.Id);
+        });
+
+        Assert.Equal("Registration does not exist.", exception.Message);
+    }
+
+    [Fact]
+    public async Task Unregister_NonExisting_Student_From_Activity()
+    {
+        // Arrange
+        var NonExistingStudent = new Guid();
+
+        // Act & Assert
+        var exception = await Assert.ThrowsAsync<ArgumentException>(async () =>
+        {
+            await _activityFacadeSUT.UnregisterStudent(ActivitiesSeeds.ICSCviko.Id, NonExistingStudent);
+        });
+
+        Assert.Equal($"Student with {NonExistingStudent} does not exist.", exception.Message);
+    }
+
+    [Fact]
+    public async Task Register_Student_For_Non_Existing_Activity()
+    {
+        // Arrange
+        var activityId = new Guid();
+
+        // Act & Assert
+        var exception = await Assert.ThrowsAsync<DbUpdateException>(async () =>
+        {
+            await _activityFacadeSUT.RegisterStudent(activityId, StudentSeeds.Terry.Id);
+        });
+    }
+
+    [Fact]
+    public async Task Unregister_Student_From_Non_Existing_Activity()
+    {
+        // Arrange
+        var activityId = new Guid();
+
+        // Act & Assert
+        var exception = await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+        {
+            await _activityFacadeSUT.UnregisterStudent(activityId, StudentSeeds.Terry.Id);
+        });
+    }
 }
+
+

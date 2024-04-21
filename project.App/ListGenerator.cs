964787e0adb5ad46c4cf02d1b2e4a528d5d4823d
AKN03 <126771174+AKN03@users.noreply.github.com>
2024-04-21 21:01:05 +0200

    Added ListGenerator.cs
    Generic Constructor for listing objects
    
    TODO:
    Needs nicer formatting
    Label above rows

---
+++ project.App/ListGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+
+namespace project.App
+{
+    public abstract class ListGenerator
+    {
+        public static void SetListView<T>(ref ListView listView, IEnumerable<T> list)
+        {
+            listView.ItemsSource = list;
+            listView.ItemTemplate = new DataTemplate(() =>
+            {
+                PropertyInfo[] properties =
+                    typeof(T).GetProperties(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public);
+
+                var grid = new Grid();
+                for (int i = 0; i < properties.Length; i++)
+                {
+                    var label = new Label();
+                    label.SetBinding(Label.TextProperty, properties[i].Name);
+                    grid.Children.Add(label);
+                    grid.SetColumn(label, i);
+                    grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                }
+
+                return new ViewCell { View = grid };
+            });
+        }
+    }
+}
+
+/*
+
+How to use
+
+CodeBehind:
+    ListGenerator.SetListView(ref subjectListView, Subjects);
+XAML:
+    <ListView x:Name="subjectListView"></ListView>
+
+( also u can edit the Formatting after or inside the function )
+
+*/

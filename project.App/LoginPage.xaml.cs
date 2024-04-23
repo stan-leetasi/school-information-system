50f39222f58a6a449dcad3215e9fee86710095da
Andrej Balatsyr <us.balatsir@gmail.com>
2024-04-24 01:35:18 +0200

    feat: draft user login page

---
+++ project.App/LoginPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using CookBook.App.ViewModels;
+
+namespace project.App;
+
+public partial class LoginPage : ContentPage
+{
+    public LoginPage()
+    {
+        InitializeComponent();
+        BindingContext = new StudentLogins();
+    }
+    
+    void OnPickerSelectedItemChanged(object sender, EventArgs e)
+    {
+        var picker = (Picker)sender;
+        var selectedStudent= (string?)picker.SelectedItem;
+
+        if (selectedStudent is not null)
+        {
+            Application.Current.MainPage = new AppShell();
+        }
+    }
+    
+    void OnAdminLogin(object sender, EventArgs e)
+    {
+        Application.Current.MainPage = new AppShell();
+    }
+}
---
+++ project.App/ViewModels/StudentLogins.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CookBook.App.ViewModels;
+
+// TODO: replace with StudentListViewModel
+public class StudentLogins
+{
+    public ObservableCollection<string> Students { get; set; }
+
+    public StudentLogins()
+    {
+        Students = new ObservableCollection<string>(GetStudentLoginsFromDatabase());
+    }
+
+    private List<string> GetStudentLoginsFromDatabase()
+    {
+        return new List<string> { "xlogin00", "xlogin01", "xlogin02" };
+    }
+}

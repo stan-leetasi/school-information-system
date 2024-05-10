1068317dc7a0c1eb8dc4ba743bb4a3346494db62
Stanislav Letaši <135342903+stan-leetasi@users.noreply.github.com>
2024-05-11 00:08:48 +0200

    feat: Added StudentDetailView page

---
+++ project.App/ViewModels/Student/StudentDetailViewModel.cs
@@ -0,0 +1,19 @@
+﻿using project.App.Services;
+using project.BL.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace project.App.ViewModels.Student
+{
+    public partial class StudentDetailViewModel : ViewModelBase
+    {
+
+        public StudentDetailModel Student { get; set; }
+
+
+        public StudentDetailViewModel(IMessengerService messengerService): base(messengerService) {
+            Student = StudentDetailModel.Empty;
+        }
+
+    }
+}

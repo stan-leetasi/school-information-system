4179b68a03bfa6ca4e7f7538b80ef5f259f640f5
Stanislav Letaši <135342903+stan-leetasi@users.noreply.github.com>
2024-05-18 14:02:38 +0200

    feat: Added button functionality

--- project.App/ViewModels/Student/StudentDetailViewModel.cs
+++ project.App/ViewModels/Student/StudentDetailViewModel.cs
@@ -1,19 +1,50 @@
 ﻿using project.App.Services;
+using project.BL.Facades;
 using project.BL.Models;
 using System.Collections.ObjectModel;
+using CommunityToolkit.Maui.Core.Extensions;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using project.App.Messages;
 using System.ComponentModel;
 
-namespace project.App.ViewModels.Student
+namespace project.App.ViewModels.Student;
+
+[QueryProperty(nameof(StudentId), nameof(StudentId))]
+public partial class StudentDetailViewModel : ViewModelBase
 {
-    public partial class StudentDetailViewModel : ViewModelBase
-    {
 
-        public StudentDetailModel Student { get; set; }
-
-
-        public StudentDetailViewModel(IMessengerService messengerService): base(messengerService) {
-            Student = StudentDetailModel.Empty;
+    public StudentDetailModel? Student { get; set; }
+    public Guid StudentId { get; set; } = new Guid();
+    public bool StudentView => _navigationService.IsStudentLoggedIn; 
+    private readonly IStudentFacade _studentFacade;
+    private readonly INavigationService _navigationService;
+        
+    public StudentDetailViewModel(IStudentFacade studentFacade,
+        INavigationService navigationService, 
+        IMessengerService messengerService): base(messengerService) 
+        {
+            _studentFacade = studentFacade;
+            _navigationService = navigationService;
         }
 
+    protected override async Task LoadDataAsync()
+    {
+        Student = await _studentFacade.GetAsync(StudentId);
     }
+
+    [RelayCommand]
+    private async Task EditStudent(Guid id)
+    {
+        //await _navigationService.GoToAsync<>;
+    }
+
+    [RelayCommand]
+    private async Task DeleteStudent(Guid id)
+    {
+        await _studentFacade.DeleteAsync(StudentId);
+        // await _navigationService.GoToAsync<>;
+    }
+
 }
+

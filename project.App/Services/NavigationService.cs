02119787cb01cf730a9c3eceb43d7a5e49a54cbd
Stanislav Letaši <135342903+stan-leetasi@users.noreply.github.com>
2024-05-18 22:42:08 +0200

    chore: Resolve merge conflict

--- project.App/Services/NavigationService.cs
+++ project.App/Services/NavigationService.cs
@@ -20,7 +20,6 @@ public class NavigationService : INavigationService
         new("//home", typeof(MainPage), typeof(ViewModelBase)),
         new("//login", typeof(LoginPage), typeof(StudentLoginViewModel)),
         new("//students", typeof(StudentListView), typeof(StudentListViewModel)),
-        new("//students/edit", typeof(StudentEditView), typeof(StudentDetailViewModel)),
         new("//subjects", typeof(SubjectListView), typeof(SubjectListViewModel)),
         new("//subjects/detail", typeof(SubjectStudentDetailView), typeof(SubjectStudentDetailViewModel)),
         new("//subjects/detail/admin", typeof(SubjectAdminDetailView), typeof(SubjectAdminDetailViewModel)),

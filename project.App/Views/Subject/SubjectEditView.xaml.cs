390d217a196e2beadf3a49797c590ff2407b38be
Jiří Tesař <xtesar44@stud.fit.vut.cz>
2024-05-20 03:44:55 +0200

    feat: Add SubjectEdit View

---
+++ project.App/Views/Subject/SubjectEditView.xaml.cs
@@ -0,0 +1,11 @@
+using project.App.ViewModels.Subject;
+
+namespace project.App.Views.Subject;
+
+public partial class SubjectEditView 
+{
+	public SubjectEditView(SubjectEditViewModel viewModel) : base(viewModel)
+	{
+		InitializeComponent();
+	}
+}

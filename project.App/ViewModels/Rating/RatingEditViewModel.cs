e61b3751555273feaf234ea89496334d5f9cc9f6
Jiří Tesař <xtesar44@stud.fit.vut.cz>
2024-05-20 03:45:50 +0200

    feat: Add RatingEdit ViewModel

---
+++ project.App/ViewModels/Rating/RatingEditViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using project.App.Messages;
+using project.App.Services;
+using project.BL.Facades;
+using project.BL.Models;
+
+namespace project.App.ViewModels.Rating;
+
+public partial class RatingEditViewModel(
+    IRatingFacade ratingFacade,
+    INavigationService navigationService,
+    IMessengerService messengerService)
+    : ViewModelBase(messengerService)
+{
+    public Guid RatingId { get; init; } = new();
+
+    public RatingDetailModel Rating { get; set; } = RatingDetailModel.Empty;
+
+    protected override async Task LoadDataAsync()
+    {
+        RatingDetailModel? getRating = await ratingFacade.GetAsync(RatingId);
+
+        if (getRating != null)
+            Rating = getRating;
+    }
+
+    [RelayCommand]
+    private async Task SaveAsync()
+    {
+        await ratingFacade.SaveAsync(Rating);
+
+        MessengerService.Send(new RatingEditMessage() { RatingId = Rating.Id });
+
+        navigationService.SendBackButtonPressed();
+    }
+}

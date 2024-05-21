fb27f9a4c1ccec9b3a3702b62604404ac86818c3
Adam Kán <126771174+AKN03@users.noreply.github.com>
2024-05-21 10:43:57 +0200

    fix: ActivityEditView Pickers working now

--- project.App/ViewModels/Activity/ActivityEditViewModel.cs
+++ project.App/ViewModels/Activity/ActivityEditViewModel.cs
@@ -20,14 +20,54 @@ public partial class ActivityEditViewModel(
     public Guid SubjectId { get; init; }
 
 
-    public ActivityAdminDetailModel Activity { get; set; } = ActivityAdminDetailModel.Empty;
+    public ActivityAdminDetailModel Activity { get; private set; } = new()
+    {
+        BeginTime = DateTime.Now,
+        EndTime = DateTime.Now,
+        Area = SchoolArea.None,
+        Type = ActivityType.None,
+        SubjectId = Guid.Empty,
+        SubjectName = string.Empty,
+        Description = string.Empty,
+    };
 
-    public List<SchoolArea> SchoolAreas { get; set; } = new((SchoolArea[])Enum.GetValues(typeof(SchoolArea)));
+    public List<SchoolArea> SchoolAreasList => Enum.GetValues(typeof(SchoolArea)).Cast<SchoolArea>().ToList();
+    public List<ActivityType> TypesList => Enum.GetValues(typeof(ActivityType)).Cast<ActivityType>().ToList();
+
 
     protected override async Task LoadDataAsync()
     {
         if (ActivityId != Guid.Empty)
             Activity = await activityFacade.GetAsync(ActivityId) ?? Activity;
+    }
+
+    public DateTime BeginDate
+    {
+        get { return Activity.BeginTime.Date; }
+        set { Activity.BeginTime = CombineDateTime(value, BeginTime); }
+    }
+
+    public DateTime EndDate
+    {
+        get { return Activity.EndTime.Date; }
+        set { Activity.EndTime = CombineDateTime(value, EndTime); }
+    }
+
+    public TimeSpan BeginTime
+    {
+        get { return Activity.BeginTime.TimeOfDay; }
+        set { Activity.BeginTime = CombineDateTime(Activity.BeginTime, value); }
+    }
+
+    public TimeSpan EndTime
+    {
+        get { return Activity.EndTime.TimeOfDay; }
+        set { Activity.EndTime = CombineDateTime(Activity.EndTime, value); }
+    }
+
+    private static DateTime CombineDateTime(DateTime date, TimeSpan time)
+    {
+        return new DateTime(date.Year, date.Month, date.Day, time.Hours, time.Minutes, time.Seconds);
     }
 
     [RelayCommand]
